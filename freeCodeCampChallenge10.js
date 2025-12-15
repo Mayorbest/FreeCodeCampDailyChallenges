52bd4d68665a4ae8249e32a5cc6ce9403e20d8f8
Mayorbest <mayowaayeni69@gmail.com>
2025-12-15 15:36:34 +0100

    Add files via upload

---
+++ freeCodeCampChallenge10.js
@@ -0,0 +1,22 @@
+/**
+ * Given the speed you are traveling in miles per hour (MPH), and a speed limit in kilometers per hour (KPH), determine whether you are speeding and if you will get a warning or a ticket.
+
+1 mile equals 1.60934 kilometers.
+If you are travelling less than or equal to the speed limit, return "Not Speeding".
+If you are travelling 5 KPH or less over the speed limit, return "Warning".
+If you are travelling more than 5 KPH over the speed limit, return "Ticket".
+Run the Tests (Ctrl + Enter)
+Reset this lesson
+Get Help
+Tests
+Waiting:1. speedCheck(30, 70) should return "Not Speeding".
+Waiting:2. speedCheck(40, 60) should return "Warning".
+Waiting:3. speedCheck(40, 65) should return "Not Speeding".
+Waiting:4. speedCheck(60, 90) should return "Ticket".
+Waiting:5. speedCheck(65, 100) should return "Warning".
+Waiting:6. speedCheck(88, 40) should return "Ticket".
+ */
+
+function speedCheck (speedMph,SpeedLtdKph) {
+    return speedMph;
+}
---
+++ mayor1.js
@@ -0,0 +1,33 @@
+console.log("Hello World!")
+/**
+ * 
+ * Given a 2D array representing the inventory of your store, and another 2D array representing a shipment you have received, return your updated inventory.
+
+Each element in the arrays will have the format: [quantity, "item"], where quantity is an integer and "item" is a string.
+Update items in the inventory by adding the quantity of any matching items from the shipment.
+If a received item does not exist in the current inventory, add it as a new entry to the end of the inventory.
+Return inventory in the order it was given with new items at the end in the order they appear in the shipment.
+For example, given an inventory of [[2, "apples"], [5, "bananas"]] and a shipment of [[1, "apples"], [3, "bananas"]], return [[3, "apples"], [8, "bananas"]].
+
+
+    1. updateInventory([[2, "apples"], [5, "bananas"]], [[1, "apples"], [3, "bananas"]]) should return [[3, "apples"], [8, "bananas"]].
+Waiting:2. updateInventory([[2, "apples"], [5, "bananas"]], [[1, "apples"], [3, "bananas"], [4, "oranges"]]) should return [[3, "apples"], [8, "bananas"], [4, "oranges"]].
+Waiting:3. updateInventory([], [[10, "apples"], [30, "bananas"], [20, "oranges"]]) should return [[10, "apples"], [30, "bananas"], [20, "oranges"]].
+Waiting:4. updateInventory([[0, "Bowling Ball"], [0, "Dirty Socks"], [0, "Hair Pin"], [0, "Microphone"]], [[1, "Hair Pin"], [1, "Half-Eaten Apple"], [1, "Bowling Ball"], [1, "Toothpaste"]]) should return [[1, "Bowling Ball"], [0, "Dirty Socks"], [1, "Hair Pin"], [0, "Microphone"], [1, "Half-Eaten Apple"], [1, "Toothpaste"]].
+ */
+
+function updateInventory (inventory, shipment) {
+    for(let i=0; i < inventory.length; i++){
+        for (let j=0; j < shipment.length; j++) {
+            if(inventory[i][1] !== shipment[j][1]){inventory.push(shipment[j])}
+            else if (inventory[i][1] === shipment[j][1]){inventory.with([i][1],shipment[j][0]+inventory[i][0])}
+            break
+        };
+    };
+    return inventory;
+};
+
+console.log(updateInventory([[2, "apples"], [5, "bananas"]],[[1, "apples"], [3, "bananas"]]))
+console.log(updateInventory([[2, "apples"], [5, "bananas"]], [[1, "apples"], [3, "bananas"], [4, "oranges"]]))
+console.log(updateInventory([], [[10, "apples"], [30, "bananas"], [20, "oranges"]]))
+console.log(updateInventory([[0, "Bowling Ball"], [0, "Dirty Socks"], [0, "Hair Pin"], [0, "Microphone"]], [[1, "Hair Pin"], [1, "Half-Eaten Apple"], [1, "Bowling Ball"], [1, "Toothpaste"]]))
